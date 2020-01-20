9e0fcb7ede04ef5e290bd847277bd19e83490b9c
wooleah <woojaeizzle@gmail.com>
2020-01-20 17:38:52 +0900

    Added searchbox

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { robots } from './robots';
+import CardList from './CardList';
+import SearchBox from './SearchBox.js';
+
+const App = () => {
+    return (
+        <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots} />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (
+        <>
+            {robots.map(robot => <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />)}
+        </>
+    );
+}
+
+export default CardList;
