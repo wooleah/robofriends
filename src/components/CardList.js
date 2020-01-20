dabb241d9f87d62fcca3249fca3c2d8ffc1cec06
wooleah <woojaeizzle@gmail.com>
2020-01-20 20:50:59 +0900

    Added errorboundaries and an error for testing

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+    if (true) {
+        throw new Error('You DIED')
+    }
     return (
         <>
             {robots.map(robot => <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />)}
